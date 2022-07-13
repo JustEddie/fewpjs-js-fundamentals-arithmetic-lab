dc9fe1fc290ccba807af3699a25bfbec4687a1eb
JustEddie <skdpwls830@gmail.com>
2022-07-13 17:21:49 +1000

    commit tree 135af6369d73f291248019fea91c467597c9afcb

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 1;
+var num2 = 62;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = parseInt(Math.random() + 1);
+
+var num3 = 9;
+var num4 = 5;
+var mod = num3 % num4;
+
+var max = Math.max(2,4,20);
